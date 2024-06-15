38f56003e46ac1c10f3fecb717bb4bb186b9f2b7
OlyaMosienko <olya.ololoeva81@mail.ru>
2024-06-15 14:31:34 +0300

    Add Click-check module

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,11 +1,40 @@
-import {Module} from '../core/module';
+import { Module } from '../core/module';
+import { randomColor } from '../utils';
 
 export class ClicksModule extends Module {
 	constructor() {
-		super('clicks', 'Посчитать количество кликов');
+		super('clicks', 'Посчитать клики (за 5 секунд)');
 	}
 
 	trigger() {
-		console.log('Подсчёт кликов');
+		let timer = 5;
+		let click = -1;
+
+		const startCheck = setInterval(() => {
+			--timer;
+
+			if (timer === 0) {
+				clearInterval(startCheck);
+				this.showScoreMessage(click);
+			}
+		}, 1000);
+
+		document.body.addEventListener('click', () => {
+			++click;
+			startCheck;
+		});
+	}
+
+	showScoreMessage(score) {
+		const scoreMessage = document.createElement('p');
+		scoreMessage.className = 'score-message';
+		scoreMessage.textContent = `Time is up, your score is ${score}!`;
+		scoreMessage.style.background = randomColor();
+
+		document.body.appendChild(scoreMessage);
+
+		setTimeout(() => {
+			scoreMessage.remove();
+		}, 3000);
 	}
 }
